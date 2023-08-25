00c49716eb080415c3e8739a5b072e3e7d1efdc3
DimitriSeaborn <dimitriseaborn@gmail.com>
2023-08-25 14:31:22 -0400

    added date

--- src/Cours02AfficherDate.java
+++ src/Cours02AfficherDate.java
@@ -1,5 +1,7 @@
 public class Cours02AfficherDate {
     public static void main(String[] args) {
+        java.util.Date date = new java.util.Date();
         System.out.println("25 août 2023");
+        System.out.println(date);
     }
 }
